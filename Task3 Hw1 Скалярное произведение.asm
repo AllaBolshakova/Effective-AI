9df76aacaee619cce09f0923d06447b3252931fe
AllaBolshakova <111781569+AllaBolshakova@users.noreply.github.com>
2026-03-15 15:12:24 +0300

    Update Task3 Hw1 Скалярное произведение.asm

--- Task3 Hw1 Скалярное произведение.asm
+++ Task3 Hw1 Скалярное произведение.asm
@@ -25,4 +25,26 @@ start:
     CMP C, [len]
     JNZ .loop
 
-HLT
+    MOV A, D
+    ; печать в консоль
+        mov D, 231
+        push A
+.loop1:
+        div 10
+        inc D
+        cmp A, 0
+        JNZ .loop1
+        
+        pop A
+.loop2:     
+        mov B, A
+        DIV 10 ; А div 10
+        MUL 10 ; А*10
+        sub B, A
+        add B, 48
+        mov [D], B
+        dec D
+        DIV 10 ; A div 10
+        cmp A, 0
+        JNZ .loop2
+	HLT
